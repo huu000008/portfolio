499e5611d2faa5c8f8ed03d6aaa7e1b1b0b2416c
johyukrae <huu000008@gmail.com>
2025-04-16 20:33:11 +0900

    fix: config 파일 수정

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compress: true,
+  turbopack: {
+    // Turbopack 관련 옵션은 객체로 작성해야 lint 에러가 발생하지 않습니다.
+    // 필요하다면 resolveAlias, resolveExtensions 등 추가 가능
+  },
   experimental: {
     typedRoutes: true,
-    turbo: {
-      // Turbopack 관련 옵션은 객체로 작성해야 lint 에러가 발생하지 않습니다.
-      // 필요하다면 resolveAlias, resolveExtensions 등 추가 가능
-    },
     optimizePackageImports: [
       // 빌드 시 패키지별 tree-shaking 최적화
       "@tanstack/react-query",
